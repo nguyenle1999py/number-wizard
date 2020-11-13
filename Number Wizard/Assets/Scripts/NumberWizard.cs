ab976db0d4ff0e7bb516e40ad4931c904748a4cc
Nguyen Le <nguyenle1999py@gmail.com>
2020-11-13 21:25:02 +0700

    Complete Game
    complete basic game: add funtion to button. add logic

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI textGuess;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        max++;
+        NextGuess();
+        Debug.Log(max.ToString());
+        Debug.Log(min.ToString());
+    }
+
+    public void PressHigher()
+    {
+        min = guess;
+        NextGuess();
+
+    }
+
+    public void PressLower()
+    {
+        max = guess;
+        NextGuess();
+
+    }
+
+    private void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        textGuess.text = guess.ToString();
+        Debug.Log(guess.ToString());
+    }
+
+    
+}
---
+++ Number Wizard/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+   
+}
+;
